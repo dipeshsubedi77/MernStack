cb6626bf27b6bf8d1ac8e127a6efdfbc7c0aa144
dipeshsubedi77 <dipeshfitness74@gmail.com>
2025-03-14 07:52:12 +0545

    hello

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
  require('dotenv').config()
  const express = require('express')
-const connectToDatabase = require('./database/Index')
+const connectToDatabase = require('./database/Index');
+const Blog = require('./database/model/blogModel');
 const app = express();
 
 app.use(express.json()); 
@@ -14,8 +15,17 @@ app.use(express.json());
     })
    })
 
-   app.post("/blog", (req, res) => {
-    console.log(req.body);
+   app.post("/blog", async (req, res) => {
+    const{title,description,subtitle,image} = req.body
+
+    await Blog.create({
+
+      title: title,
+      description : description,
+      image: image,
+      subtitle: subtitle
+
+    })
     res.status(200).json({
         message: "Blog is ready",
     });
